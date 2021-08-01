8f023169119ab82408f39c7083e68d1895a03038
KulveerBrar <brarkulveer15@gmail.com>
2021-07-31 21:10:05 -0700

    set alert on empty search closes #5

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -12,23 +12,24 @@ const Home = () => {
     };
 
     const searchChannel = async () => {
-        const result = await getChannel(search)
-        setChannels(result)
+        !search ? alert("Please enter Channel Name") :
+            setChannels(await getChannel(search))
+
     }
     return (
         <>
-        <Form className="d-flex">
-            <FormControl
-                type="search"
-                placeholder="Search"
-                className="mr-2"
-                aria-label="Search"
-                onChange={handleSearchChange}
-            />
-            <Button variant="outline-primary" onClick={searchChannel}>Search</Button>
-        </Form>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="mr-2"
+                    aria-label="Search"
+                    onChange={handleSearchChange}
+                />
+                <Button variant="outline-primary" onClick={searchChannel}>Search</Button>
+            </Form>
 
-        {channels.map((a, index) => (<p key={index}>{a.display_name}</p>))}
+            {!channels ? <p> No Results Found </p> : channels.map((a, index) => (<p key={index}>{a.display_name}</p>))}
         </>
     )
 }
